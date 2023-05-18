fa6867ea56992f31ab7d7e8bfe2c14c5f626be22
Stella Srzich <stella.srzich@gmail.com>
2023-05-18 23:52:01 +1200

    fix pipeline now?

--- src/cosc202/andie/EmbossFilter.java
+++ src/cosc202/andie/EmbossFilter.java
@@ -103,7 +103,7 @@ public class EmbossFilter implements ImageOperation, java.io.Serializable {
      * </p>
      * @see GaussianBlurFilter
      * @param removeNoise True to apply a light Gaussian blur filter before the sobel filter, false otherwise.
-     * @param embosstype Determines the direction of the emboss. The options are ints 1 to 8 (inclusive).
+     * @param embossType Determines the direction of the emboss. The options are ints 1 to 8 (inclusive). See the EMBOSS_N public static ints of this class.
      */
     public EmbossFilter(boolean removeNoise, int embossType) {
         this.removeNoise = removeNoise;
