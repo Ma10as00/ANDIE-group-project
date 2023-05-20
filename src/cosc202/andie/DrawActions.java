0cbf714a5c3c849326c0474a34440bce19425e40
James Liu <liuja576@student.otago.ac.nz>
2023-05-20 18:32:24 +1200

    idk what im doing

--- src/cosc202/andie/DrawActions.java
+++ src/cosc202/andie/DrawActions.java
@@ -85,87 +85,86 @@ public class DrawActions {
         }
 
         public void actionPerformed(ActionEvent e) {
-            // try{
+            try{
 
-            // JPanel choosePanel = new JPanel(new GridLayout(0, 1));
-            // JCheckBox colourBox = new JCheckBox("Colour");
-            // choosePanel.add(colourBox);
+            JPanel choosePanel = new JPanel(new GridLayout(0, 1));
+            JCheckBox colourBox = new JCheckBox("Colour");
+            choosePanel.add(colourBox);
 
-            // int chooseOption = JOptionPane.showOptionDialog(null, choosePanel, "Colour
-            // Rectangle?", JOptionPane.OK_CANCEL_OPTION, JOptionPane.CANCEL_OPTION, null,
-            // null, chooseOption);
-            // if(chooseOption == JOptionPane.CANCEL_OPTION){
-            // return;
-            // }else if(chooseOption == JOptionPane.OK_CANCEL_OPTION){
-            // target.getImage().undo();
-            // //Andie.RegionSelection = false;
+            int chooseOption = JOptionPane.showOptionDialog(null, choosePanel, "Colour
+            Rectangle?", JOptionPane.OK_CANCEL_OPTION, JOptionPane.CANCEL_OPTION, null,
+            null, chooseOption);
+            if(chooseOption == JOptionPane.CANCEL_OPTION){
+            return;
+            }else if(chooseOption == JOptionPane.OK_CANCEL_OPTION){
+            target.getImage().undo();
+            //Andie.RegionSelection = false;
 
-            // //if(colourBox.isSelected()) target.getImage().apply(new
-            // DrawRec(Andie.mouseSec, ColourActions.activeColour , chooseOption, enabled));
+            //if(colourBox.isSelected()) target.getImage().apply(new
+            DrawRec(Andie.mouseSec, ColourActions.activeColour , chooseOption, enabled));
 
-            // target.repaint();
-            // target.getParent().revalidate();
-            // }
-            // }
-            // }
-        }
-    }
-
-    /**
-     * <p>
-     * Action to draw circle
-     * </p>
-     * 
-     * @see DrawRec
-     */
-    public class DrawCircleAction extends ImageAction {
-
-        /**
-         * <p>
-         * Create a new draw circle action.
-         * </p>
-         * 
-         * @param name     The name of the action (ignored if null).
-         * @param icon     An icon to use to represent the action (ignored if null).
-         * @param desc     A brief description of the action (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
-         */
-        DrawCircleAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
-            super(name, icon, desc, mnemonic);
-        }
-
-        public void actionPerformed(ActionEvent e) {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-        }
-    }
-
-    /**
-     * <p>
-     * Action to draw line
-     * </p>
-     * 
-     * @see DrawLine
-     */
-    public class DrawLineAction extends ImageAction {
-
-        /**
-         * <p>
-         * Create a new draw line action.
-         * </p>
-         * 
-         * @param name     The name of the action (ignored if null).
-         * @param icon     An icon to use to represent the action (ignored if null).
-         * @param desc     A brief description of the action (ignored if null).
-         * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
-         */
-        DrawLineAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
-            super(name, icon, desc, mnemonic);
-        }
-
-        public void actionPerformed(ActionEvent e) {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-        }
+            target.repaint();
+            target.getParent().revalidate();
+            }
+            }
+            }
     }
 }
+
+/**
+ * <p>
+ * Action to draw circle
+ * </p>
+ * 
+ * @see DrawRec
+ */
+public class DrawCircleAction extends ImageAction {
+
+    /**
+     * <p>
+     * Create a new draw circle action.
+     * </p>
+     * 
+     * @param name     The name of the action (ignored if null).
+     * @param icon     An icon to use to represent the action (ignored if null).
+     * @param desc     A brief description of the action (ignored if null).
+     * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+     */
+    DrawCircleAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+        super(name, icon, desc, mnemonic);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+}
+
+/**
+ * <p>
+ * Action to draw line
+ * </p>
+ * 
+ * @see DrawLine
+ */
+public class DrawLineAction extends ImageAction {
+
+    /**
+     * <p>
+     * Create a new draw line action.
+     * </p>
+     * 
+     * @param name     The name of the action (ignored if null).
+     * @param icon     An icon to use to represent the action (ignored if null).
+     * @param desc     A brief description of the action (ignored if null).
+     * @param mnemonic A mnemonic key to use as a shortcut (ignored if null).
+     */
+    DrawLineAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+        super(name, icon, desc, mnemonic);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+}}
