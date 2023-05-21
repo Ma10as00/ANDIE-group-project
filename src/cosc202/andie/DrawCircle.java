a9c764c69be4c1ed782923362fa7fff21ce6fd3c
“winka826” <winka826@student.otago.ac.nz>
2023-05-21 20:32:31 +1200

    Outlines and width

--- src/cosc202/andie/DrawCircle.java
+++ src/cosc202/andie/DrawCircle.java
@@ -2,6 +2,7 @@ package cosc202.andie;
 
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
+import java.awt.BasicStroke;
 import java.awt.Color;
 
 public class DrawCircle implements ImageOperation, java.io.Serializable {
@@ -11,20 +12,30 @@ public class DrawCircle implements ImageOperation, java.io.Serializable {
     int y;
     int width;
     int height;
+    boolean fill;
 
-    DrawCircle(int x, int y, int height, int width) {
+    DrawCircle(int x, int y, int height, int width, boolean fill) {
         this.x = x;
         this.y = y;
         this.height = height;
         this.width = width;
+        this.fill = fill;
         col = DrawActions.userColour;
     }
 
     @Override
     public BufferedImage apply(BufferedImage input) {
         Graphics2D g = input.createGraphics();
-        g.setColor(col);
-        g.fillOval(x, y, width, height);
+        if (!fill) {
+            g.setColor(col);
+            g.fillOval(x, y, width, height);
+        }
+
+        if (fill) {
+            g.setColor(DrawActions.userColour);
+            g.setStroke(new BasicStroke((float) DrawActions.userWidth));
+            g.drawOval(x, y, width, height);
+        }
         g.dispose();
         return input;
     }
--- src/cosc202/andie/DrawRec.java
+++ src/cosc202/andie/DrawRec.java
@@ -2,6 +2,7 @@ package cosc202.andie;
 
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Rectangle;
 
@@ -9,17 +10,28 @@ public class DrawRec implements ImageOperation, java.io.Serializable {
 
     private Rectangle r;
     private Color col;
+    private boolean fill;
 
-    DrawRec(Rectangle rect, Color userColor) {
+    DrawRec(Rectangle rect, Color userColor, Boolean fill) {
         r = rect;
         col = userColor;
+        this.fill = fill;
     }
 
     @Override
     public BufferedImage apply(BufferedImage input) {
         Graphics2D g2d = (Graphics2D) input.getGraphics();
-        g2d.setColor(col);
-        g2d.fill(r);
+        if (!fill) {
+            g2d.setColor(col);
+            g2d.fill(r);
+        }
+
+        if (fill) {
+            g2d.setColor(DrawActions.userColour);
+            BasicStroke stroke = new BasicStroke((float) DrawActions.userWidth);
+            g2d.setStroke(stroke);
+            g2d.draw(r);
+        }
         g2d.dispose();
         return input;
     }
