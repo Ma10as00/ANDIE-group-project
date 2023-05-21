29c30e13a03293a7a6060ea1723f82be49af2175
James Liu <liuja576@student.otago.ac.nz>
2023-05-21 21:26:26 +1200

    done more drawing

--- src/cosc202/andie/DrawLine.java
+++ src/cosc202/andie/DrawLine.java
@@ -4,17 +4,10 @@ import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
 import java.awt.BasicStroke;
 import java.awt.Color;
-<<<<<<< HEAD
 import java.awt.RenderingHints;
 import java.awt.Point;
 import java.awt.LinearGradientPaint;
-=======
->>>>>>> 73e0992777e09832da46f0910a136126c8e96aa0
 
-/**
- * An image operation that draws pixels to the screen in an array with a
- * selected
- */
 public class DrawLine implements ImageOperation, java.io.Serializable {
 
     private Color col;
@@ -23,34 +16,20 @@ public class DrawLine implements ImageOperation, java.io.Serializable {
     int exitX;
     int exitY;
 
-<<<<<<< HEAD
     DrawLine(int enterX, int enterY, int exitX, int exitY) {
         this.enterX = enterX;
         this.enterY = enterY;
         this.exitX = exitX;
         this.enterY = enterY;
         col = DrawActions.userColour;
-=======
-    /**
-     * Image operation that draws a line over the image
-     * 
-     * 
-     */
-    DrawLine() {
->>>>>>> 73e0992777e09832da46f0910a136126c8e96aa0
 
     }
 
     @Override
     public BufferedImage apply(BufferedImage input) {
-<<<<<<< HEAD
         Graphics2D g = (Graphics2D) input.getGraphics();
         g.setColor(col);
         g.drawLine(enterX, enterY, exitX, enterY);
-=======
-
-        // Return the image
->>>>>>> 73e0992777e09832da46f0910a136126c8e96aa0
         return input;
     }
 
