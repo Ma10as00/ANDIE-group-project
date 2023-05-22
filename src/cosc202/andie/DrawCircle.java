84bc2ec84979b5f0be4c19d2c349e3a8497c66dc
James Liu <liuja576@student.otago.ac.nz>
2023-05-23 10:09:43 +1200

    comments and formating

--- src/cosc202/andie/DrawCircle.java
+++ src/cosc202/andie/DrawCircle.java
@@ -26,8 +26,13 @@ import java.awt.Color;
  * 
  * @author Katie Wink
  */
+
+// The class represents a circle-drawing operation on an image.
+// It implements the ImageOperation interface to define the apply() method.
+// The class is also Serializable, indicating that its instances can be serialized.
 public class DrawCircle implements ImageOperation, Serializable {
 
+    // Private instance variables for storing the color, center coordinates, width, height, fill status, and stroke.
     private Color col;
     private int x;
     private int y;
@@ -36,30 +41,52 @@ public class DrawCircle implements ImageOperation, Serializable {
     private boolean fill;
     private BasicStroke stroke;
 
+    // Constructor for the DrawCircle class.
+    // It takes a scale value, center coordinates, width, height, and a boolean value indicating whether the circle should be filled or not.
     public DrawCircle(double scale, int x, int y, int height, int width, boolean fill) {
+
+        // Scaling the center coordinates, width, and height based on the provided scale value.
+        // The x, y, width, and height values of the circle are divided by the scale to obtain scaled values.
+        // Setting the fill status based on the provided boolean value.
         this.x = (int) ((double) x / scale);
         this.y = (int) ((double) y / scale);
         this.height = (int) ((double) height / scale);
         this.width = (int) ((double) width / scale);
         this.fill = fill;
+
+        // The stroke width is calculated by dividing DrawActions.userWidth by the scale.
+        // Creating a BasicStroke object for the stroke of the circle.
         this.stroke = new BasicStroke((int) (DrawActions.userWidth / scale));
+        // Creating a Color object based on the RGB values obtained from DrawActions.userColour.
         col = new Color(DrawActions.userColour.getRed(), DrawActions.userColour.getGreen(),
                 DrawActions.userColour.getBlue());
     }
 
     @Override
+    // Implementation of the apply() method from the ImageOperation interface.
+    // It takes a BufferedImage as input and returns the modified image.
     public BufferedImage apply(BufferedImage input) {
+        // Creating a Graphics2D object based on the input BufferedImage.
         Graphics2D g = input.createGraphics();
+
+        // If the circle is not supposed to be filled:
+        // Set the color on the Graphics2D object.
+        // Fill the circle with the specified color.
         if (!fill) {
             g.setColor(col);
             g.fillOval(x, y, width, height);
         }
+        // If the circle is supposed to be filled:
+        // Set the color and stroke on the Graphics2D object.
+        // Draw the outline of the circle using the specified color and stroke.
         if (fill) {
             g.setColor(this.col);
             g.setStroke(stroke);
             g.drawOval(x, y, width, height);
         }
+        // Releasing system resources associated with the Graphics2D object.
         g.dispose();
+        // Returning the modified BufferedImage.
         return input;
     }
 
--- src/cosc202/andie/DrawRec.java
+++ src/cosc202/andie/DrawRec.java
@@ -13,39 +13,63 @@ import java.awt.Rectangle;
  * 4.0</a>
  * </p>
  */
-
+// The class represents a rectangle-drawing operation on an image.
+// It implements the ImageOperation interface to define the apply() method.
+// The class is also Serializable, indicating that its instances can be serialized.
 public class DrawRec implements ImageOperation, Serializable {
 
+    // Private instance variables for storing the rectangle, color, fill status, and stroke.
     private Rectangle r;
     private Color col;
     private boolean fill;
     private BasicStroke stroke;
 
+    // Constructor for the DrawRec class.
+    // It takes a scale value, a rectangle object, and a boolean value indicating whether the rectangle should be filled or not.
     public DrawRec(double scale, Rectangle rect, Boolean fill) {
+        // Scaling the rectangle coordinates and dimensions based on the provided scale value.
+        // The x, y, width, and height values of the rectangle are divided by the scale to obtain scaled values.
         int x = (int) ((double) rect.x / scale);
         int y = (int) ((double) rect.y / scale);
         int width = (int) ((double) rect.width / scale);
         int height = (int) ((double) rect.height / scale);
+        
+        // Creating a BasicStroke object for the stroke of the rectangle.
+        // The stroke width is calculated by dividing DrawActions.userWidth by the scale.
+        // Creating a Rectangle object with the scaled coordinates and dimensions.
+        // Creating a Color object based on the RGB values obtained from DrawActions.userColour.
+        // Setting the fill status based on the provided boolean value.
         this.stroke = new BasicStroke((int) (DrawActions.userWidth / scale));
         r = new Rectangle(x, y, width, height);
         col = new Color(DrawActions.userColour.getRed(), DrawActions.userColour.getGreen(),
                 DrawActions.userColour.getBlue());
         this.fill = fill;
     }
-
+    // Implementation of the apply() method from the ImageOperation interface.
+    // It takes a BufferedImage as input and returns the modified image.
     @Override
     public BufferedImage apply(BufferedImage input) {
+        // Creating a Graphics2D object based on the input BufferedImage.
         Graphics2D g2d = (Graphics2D) input.getGraphics();
+
+        // If the rectangle is not supposed to be filled:
+        // Set the color on the Graphics2D object.
+        // Fill the rectangle with the specified color.
         if (!fill) {
             g2d.setColor(this.col);
             g2d.fill(r);
         }
+        // If the rectangle is supposed to be filled:
+        // Set the color and stroke on the Graphics2D object.
+        // Draw the outline of the rectangle using the specified color and stroke.
         if (fill) {
             g2d.setColor(this.col);
             g2d.setStroke(stroke);
             g2d.draw(r);
         }
+        // Releasing system resources associated with the Graphics2D object.
         g2d.dispose();
+        // Returning the modified BufferedImage.
         return input;
     }
 
